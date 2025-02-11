9a4bbeda83f6f5e4e11f43a25e930d73c2eb7d5a
Finn Sadler <finnsadler30@gmail.com>
2025-02-11 12:32:50 +0100

    Update Extract_And_Preprocess.py

--- Extract_And_Preprocess.py
+++ Extract_And_Preprocess.py
@@ -4,21 +4,17 @@ import PyPDF2
 def Extract_And_Preprocess(pdf_path, skip_pages):
     """
     This function should be used BEFORE passing text to spaCy so sentences can be detected.
-
-
+    
     Takes PDF, reads it into Python as text and cleans it:
         - Removes line breaks
         - Removes bullet points
         - Removes page numbers
         - Removes trailing whitespace
         - Removes double spaces
-   
+        
     Args
         - (pdf_path) = path to pdf that you want to extract and clean
         - (skip_pages) = number of pages skipped before extracting
-
-
-   
     """
     with open(pdf_path, 'rb') as pdf_file:
         reader = PdfReader(pdf_file)
